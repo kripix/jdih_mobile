1fea5c0d3b88835f5d20941e522757080717b8d4
KUHAKU-tech <55788879+KUHAKU-tech@users.noreply.github.com>
2024-04-17 12:39:30 +0700

    feat: main & bottomnavbar

--- app/src/main/java/com/krispy/kelompok1_jdih/MainActivity.kt
+++ app/src/main/java/com/krispy/kelompok1_jdih/MainActivity.kt
@@ -5,16 +5,40 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.krispy.kelompok1_jdih.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        replaceFragment(HomeFragment())
+        binding.bottomNav.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.home_icon -> replaceFragment(HomeFragment())
+                R.id.docs_icon -> replaceFragment(DocsFragment())
+                R.id.account_icon-> replaceFragment(AccountFragment())
+                else -> {
+                }
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.main,fragment)
+        fragmentTransaction.commit()
     }
 }
